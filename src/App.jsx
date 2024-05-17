70dfe182f07a3352ba4442b0056a0e4803a39a43
riccelle011 <jr7263857@gmail.com>
2024-05-17 08:31:57 -0300

    retomada

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => (
+  <>
+    <section>
+      <header>
+  AULA INICIAL DE REACTJS
+   </header>
+  <content>
+    Vamos codar em reactJS
+    </content>
+    <footer>
+      @itb2024
+     </footer>
+      </section>
+   </>
+)
+
+const App = () =>
+
+export default App
